1905a184b460628ddc1130979ce122934edb8dec
chayapatsam <chayapat.sam@dome.tu.ac.th>
2024-04-28 17:01:47 +0700

    edit navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,38 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function Navbar(){
-  return(
+export default function Navbar() {
+  const router = useRouter();
+
+  // Helper function to determine if the link is active
+  const isActive = (path) => {
+    return router.pathname === path;
+  };
+
+  return (
     <nav className="bg-[#374536] py-4 flex items-center justify-between">
       <div className="space-x-6 ml-8">
-        <Link href="/" className="text-white hover:text-green-500 transition duration-300 border-b border-transparent hover:border-green-500">Home</Link>
-        <Link href="/shop" className="text-white hover:text-green-500 transition duration-300 border-b border-transparent hover:border-green-500">Shop</Link>
-        <Link href="/about" className="text-white hover:text-green-500 transition duration-300 border-b border-transparent hover:border-green-500">About</Link>
-        <Link href="/contact" className="text-white hover:text-green-500 transition duration-300 border-b border-transparent hover:border-green-500">Contact</Link>
+        <Link href="/" passHref>
+          <span className={`cursor-pointer text-white  ${isActive('/') ? 'border-b-2 border-white' : 'hover:text-white-500 border-b border-transparent '}`}>Home</span>
+        </Link>
+        <Link href="/shop" passHref>
+          <span className={`cursor-pointer text-white  ${isActive('/shop') ? 'border-b-2 border-white' : 'hover:text-white-500 border-b border-transparent '}`}>Shop</span>
+        </Link>
+        <Link href="/about" passHref>
+          <span className={`cursor-pointer text-white  ${isActive('/about') ? 'border-b-2 border-white' : 'hover:text-white-500 border-b border-transparent '}`}>About</span>
+        </Link>
+        <Link href="/contact" passHref>
+          <span className={`cursor-pointer text-white   ${isActive('/contact') ? 'border-b-2 border-white' : 'hover:text-white-500 border-b border-transparent '}`}>Contact</span>
+        </Link>
       </div>
       <div className="pr-8">
-        <Link href="/cart"><Image src="/cart.png" width={40} height={450} alt="logo"/></Link>
+        <Link href="/cart" passHref>
+          <span className="cursor-pointer">
+            <Image src="/cart.png" width={40} height={40} alt="cart" />
+          </span>
+        </Link>
       </div>
     </nav>
-  )
+  );
 }
